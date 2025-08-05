3c2ea835736ffd2471acf7b156f6b0558eec54c9
EliasTSM <eliastiago46@gmail.com>
2025-08-05 19:31:07 -0300

    mongo URI

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(String(process.env.MONGO_URI)),
     PostagensModule,
     JwtModule.register({
       global: true,
